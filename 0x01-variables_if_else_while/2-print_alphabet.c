392fc68d0be7c160c88c312c8c2ce2f18661f8bc
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-24 19:18:05 -0700

    program to change alphabet to lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 #include <ctype.h>
+
 /**
  * main - Entry point
- * Description: to change lower acse to upper case
- * Return: Always 0 (Sucess)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
-{
-	char str[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+	char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < strlen(str); i++)
 	{
 		str[i] = tolower(str[i]);
-		return (0);
+		putchar(str);
 	}
-
-	printf("%s", str);
 	return (0);
 }
