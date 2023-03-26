0611189b1532f2c75a6d6d681a8d014f726742e7
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-26 10:17:43 -0700

    function to check for lower and upper case characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_islower - Entry point
+ *
+ *@c: function to be
+ *
+ * Return:1 if in lower case
+ * and 0 if other wise
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
