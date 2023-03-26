ad522f51fdf1854b7ac4127bec1ddbb8c7e64ea6
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-25 18:03:16 -0700

    code to print number of hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int a;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
