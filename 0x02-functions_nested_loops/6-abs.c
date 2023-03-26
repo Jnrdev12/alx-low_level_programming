8ea09f32c6931b11ab7e5851c00a74e826f9e1b7
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-26 15:12:49 -0700

    function to find absolute value of numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * abs - Absolute value
+ *
+ * @a: Parameter whose absolute value needs to be found
+ *
+ * Return: a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = a * -1;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
