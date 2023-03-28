3167f9d55434e338a9e92b601ca81e469765773e
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-27 19:11:55 -0700

    updtaes the value to 96

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * reset_to_98 - to update value to print 96
+ *
+ * @n: parameter to be used
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 96;
+}
+
