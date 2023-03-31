231900dd01ea65689ed9071c2aa33dd476b1058a
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-31 14:35:57 -0700

    to rpint half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - to print half of a string
+ *
+ * @s:  variable to be used
+ *
+ * Return 0
+ */
+void puts_half(char *s)
+{
+	int len = strlen(s);
+
+	if (len % 2 == 0)
+	{
+		int n = len / 2;
+		int i = n;
+
+		for (; i < len; i++)
+		{
+			printf("%c", *(s + i));
+		}
+		putchar('\n');
+	}
+	else
+	{
+		int n = (len - 1 / 2);
+		int i = n;
+
+		for (; i < len; i++)
+		{
+			printf("%c", *(s + i));
+		}
+		_putchar('\n');
+	}
+}
