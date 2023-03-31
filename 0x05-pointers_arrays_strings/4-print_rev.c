60fba3c85475cca7b6967e1cb29344a198c9c1b7
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-03-30 22:11:41 -0700

    to reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,18 +4,21 @@
 /**
  * print_rev -  to print the reverse of a string
  *
- * @str-variable to be used
+ * @s-variable to be used
  *
  * Return 0
  */
 void print_rev(char *s)
 {
-	int len =strlen(s);
-	int i;
-	for (i = len - 1 ; i >= 0; i--)
+	int len = strlen(s);
+	int i = len - 1;
+
+	for (; i >= 0; i--)
 	{
-		char r = *(s+i);
-		_putchar(r);;
+		char r = *(s + i);
+
+		_putchar(r);
+
 	}
 	_putchar('\n');
 }
