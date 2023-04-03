294253fe88c2162ff78b9969abf8de145fcd432f
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-04-02 19:12:15 -0700

    Program to show if is a uppercae character

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void print_rev(char *s);
+void puts_half(char *str);
+void strlen_noSpace(char *s);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+ int _isupper(int c);
