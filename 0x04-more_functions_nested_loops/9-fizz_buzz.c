e7a09977935cbd928ae5ed47a89cb4aed5ef3317
Jnrdev12 <oyedejihiqmat@gmail.com>
2023-04-03 18:05:45 -0700

    fizzbuzz project

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: FizzBuzz Project:wq
+ *
+ *Return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d ", i);
+		}
+		else if (i % 3 == 0 && i % 5 ==0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
